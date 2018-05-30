06b5dcf25033fa67b857e79ba6803cf72d27482f
Nelson Cooper <ncooper1106@gmail.com>
2018-05-30 11:32:31 -0700

    deleted extra files

--- build.py
+++ build.py
@@ -1,22 +1,30 @@
-top = open('templates/top.html').read()
-bottom = open('templates/bottom.html').read()
+def main():
+  #load header and footer of website
+  top = open('templates/top.html').read()
+  bottom = open('templates/bottom.html').read()
+  
+  #creates the index page
+  index = open('content/index.html').read()
+  index = top + index + bottom
+  open('docs/index.html', 'w+').write(index)
 
+  #creates the contact page
+  contact = open('content/contact.html').read()
+  contact = top + contact + bottom
+  open('docs/contact.html', 'w+').write(contact)
 
-index = open('content/index.html').read()
-index = top + index + bottom
-open('docs/index.html', 'w+').write(index)
+  #creates the education page
+  education = open('content/education.html').read()
+  education = top + education + bottom
+  open('docs/education.html', 'w+').write(education)
 
-contact = open('content/contact.html').read()
-contact = top + contact + bottom
-open('docs/contact.html', 'w+').write(contact)
-
-education = open('content/education.html').read()
-education = top + education + bottom
-open('docs/education.html', 'w+').write(education)
-
-experience = open('content/experience.html').read()
-experience = top + experience + bottom
-open('docs/experience.html', 'w+').write(experience)
+  #creates the experience page
+  experience = open('content/experience.html').read()
+  experience = top + experience + bottom
+  open('docs/experience.html', 'w+').write(experience)
+  
+if __name__ == '__main__':
+  main()
 
 
 
--- build.sh
+++
@@ -1,4 +0,0 @@
-cat templates/top.html content/index.html templates/bottom.html > docs/index.html
-cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
-cat templates/top.html content/education.html templates/bottom.html > docs/education.html
-cat templates/top.html content/experience.html templates/bottom.html > docs/experience.html
